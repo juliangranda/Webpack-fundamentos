75af54f806e24179810a4f7fcf739169ed144109
juliangranda <juliangranda@hotmail.com>
2022-01-12 17:27:04 -0500

    webpack alias, como atajos o legibilidad para nuestros path

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,13 @@ module.exports = {
     },
     //extensiones
     resolve: {
-        extensions: ['.js']
+        extensions: ['.js'],
+        alias: {
+          '@utils': path.resolve(__dirname,'src/utils/'),
+          '@templates': path.resolve(__dirname,'src/templates/'),
+          '@styles': path.resolve(__dirname,'src/styles/'),
+          '@images': path.resolve(__dirname,'src/assets/images/')
+        }
     },
     module: {
         rules: [
@@ -52,7 +58,7 @@ module.exports = {
                 mimetype:"application/font-woff",
                 name:"[name].[contenthash].[ext]",
                 outputPath: "./assets/fonts/",
-                publicPath:"./assets/fonts/",
+                publicPath:"../assets/fonts/",
                 esModule:false,
               }
             }
